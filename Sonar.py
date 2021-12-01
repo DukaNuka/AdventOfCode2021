c3ded4b20960505f632e47fd0dcb9945c7315c9c
FlorianKrieghoff <florian.krieghoff@fintiba.com>
2021-12-01 14:46:31 +0100

    Solution for 2021 day 1 task 1 and 2

--- Sonar.py
+++ Sonar.py
@@ -3,8 +3,19 @@ class Sonar(object):
         self._depth = None
         self._deeper = 0
         self._shallower = 0
+        self._depth_sum = 0
+        self._sum_deeper = 0
+        self._sum_shallower = 0
         self._last_deeper = None
         self._history = []
+
+    @property
+    def sum_deeper( self ):
+        return self._sum_deeper
+
+    @property
+    def sum_shallower( self ):
+        return self._sum_shallower
 
     @property
     def deeper(self):
@@ -27,17 +38,26 @@ class Sonar(object):
         return self._depth
 
     @depth.setter
-    def depth(self, value):
-        self.history.append(value)
+    def depth( self, new_depth ):
+        self.compare_sums( new_depth )
         if self._depth is None:
-            self._depth = value
-        elif value > self._depth:
+            self._depth = new_depth
+        elif new_depth > self._depth:
             self._deeper += 1
             self._last_deeper = True
-        elif value < self._depth:
+        elif new_depth < self._depth:
             self._shallower += 1
             self._last_deeper = False
-        self._depth = value
+        self._depth = new_depth
+        self.history.append( new_depth )
+        self._depth_sum = sum(self.history[-3:])
+
+    def compare_sums( self, new_depth ):
+        if len(self.history) > 2:
+            if sum(self.history[-2:]) + new_depth > sum(self.history[-3:]):
+                self._sum_deeper+=1
+            else:
+                self._sum_shallower+=1
 
     def analyze_new_depth( self, new_depth ):
         self.depth = int(new_depth)
