f4111ed4f1b47152d4f8dfa764e4ba1671408aa2
FlorianKrieghoff <florian.krieghoff@fintiba.com>
2021-12-03 12:48:01 +0100

    Added Day3task2 solution and lots of debugging stuff

--- Diagnostic.py
+++ Diagnostic.py
@@ -33,3 +33,58 @@ class Diagnostic:
     def epsilon(self):
         gamma = self.gamma()
         return gamma ^ int("111111111111", 2)
+
+    def search_oxygen_reading(self):
+        values = self._history.copy()
+        return self.get_oxygen_reading(values, 0)
+
+    def get_oxygen_reading(self, values, bit):
+        sub_diag = Diagnostic()
+        new_values = []
+        keys = sorted(self.ones.keys(), reverse=True)
+        for value in values:
+            sub_diag.analyze(value)
+        if sub_diag.ones[keys[bit]] >= len(sub_diag._history) / 2:
+            for value in values:
+                if (value >> keys[bit]) & 1 == 1:
+                    new_values.append(value)
+        else:
+            for value in values:
+                if (value >> keys[bit]) & 1 == 0:
+                    new_values.append(value)
+        bit += 1
+        return (
+            new_values
+            if bit >= len(keys) or len(new_values) == 1
+            else self.get_oxygen_reading(new_values, bit)
+        )
+
+    def search_carbon_reading(self):
+        values = self._history.copy()
+        return self.get_carbon_reading(values, 0)
+
+    def get_carbon_reading(self, values, bit):
+        sub_diag = Diagnostic()
+        new_values = []
+        keys = sorted(self.ones.keys(), reverse=True)
+        for value in values:
+            sub_diag.analyze(value)
+        zeros = len(sub_diag._history) - sub_diag.ones[keys[bit]]
+        if zeros <= (len(sub_diag._history) / 2):
+            for value in values:
+                if (value >> keys[bit]) & 1 != 1:
+                    # print(f"({bin(value)} >> {keys[bit]} ^ 0) == 1")
+                    new_values.append(value)
+        else:
+            for value in values:
+                if (value >> keys[bit]) & 1 != 0:
+                    # print(f"({bin(value)} >> {keys[bit]} ^ 0) == 0")
+                    new_values.append(value)
+        bit += 1
+        # print(bit)
+        # print(values)
+        return (
+            new_values
+            if bit >= len(keys) or len(new_values) == 1
+            else self.get_carbon_reading(new_values, bit)
+        )
