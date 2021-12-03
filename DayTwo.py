85f8c327d5c0901ff23ea71c4bf3411656971857
FlorianKrieghoff <florian.krieghoff@fintiba.com>
2021-12-03 09:21:06 +0100

    moved day2task1 into its own branch so that the old results are always accessible

--- DayTwo.py
+++ DayTwo.py
@@ -4,4 +4,4 @@ from Submarine import Submarine
 
 submarine = Submarine()
 InputReader.read_file_to_function("DayTwoInput.txt", submarine.parse_line )
-print(f"Solution task 1: {submarine.latitude * submarine.current_depth}")
+print(f"Solution task 1: {submarine.latitude * submarine.current_depth}") #change for commit
